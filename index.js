228c71874821d0c7213881e5966432ccc82dc09c
ulloagabriel <ulloasaavedragabriel@gmail.com>
2022-10-04 13:48:17 -0300

    mostly worked on js the grid is reset properly when user sets the size of it

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const btnBlack = document.querySelector('.black');
 const btnRainbow = document.querySelector('.rainbow');
 const btnPickColor = document.querySelector('.pick');
 const btnErase = document.querySelector('.erase');
+const btnSize = document.querySelector('.size');
 const divItem = document.querySelector('.container > div');
-function createGrid() {
-    const size = 16*16;
-    for(let i=0; i<size; i+=1) {
+const defaultSize = 16;
+function createGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    for(let i=0; i<size * size; i+=1) {
         const div = document.createElement('div');
         gridContainer.appendChild(div);
         div.addEventListener('mouseover', ()=> {
@@ -14,4 +16,13 @@ function createGrid() {
         })
     }
 };
-createGrid();
+// this function is very important, here I learn how to reset values from what really is on the .html file
+function resetGrid() {
+    gridContainer.innerHTML = '';
+};
+createGrid(defaultSize);
+btnSize.addEventListener('click', ()=> {
+    chosenSize = prompt('What size do you prefer the square grid?', 16);
+    resetGrid();
+    createGrid(chosenSize);
+})

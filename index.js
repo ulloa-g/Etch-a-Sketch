40c3c037e153a704e362e501e0790f794bfdb2be
ulloagabriel <ulloasaavedragabriel@gmail.com>
2022-10-04 15:30:11 -0300

    btn default rainbow as red and size are working fine time to make the real rainbow efect

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const gridContainer = document.querySelector('.container');
-const btnBlack = document.querySelector('.black');
+const btnDefault = document.querySelector('.default');
 const btnRainbow = document.querySelector('.rainbow');
 const btnPickColor = document.querySelector('.pick');
 const btnErase = document.querySelector('.erase');
 const btnSize = document.querySelector('.size');
 const divItem = document.querySelector('.container > div');
 const defaultSize = 16;
+const defaultColor = '#252422';
 function createGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
     for(let i=0; i<size * size; i+=1) {
         const div = document.createElement('div');
         gridContainer.appendChild(div);
+        color = defaultColor;
+        btnRainbow.addEventListener('click', ()=> {
+            div.addEventListener('mouseover', ()=> {
+                div.style.backgroundColor = 'red';
+            })
+        });
+        btnDefault.addEventListener('click', ()=> {
+            div.addEventListener('mouseover', ()=> {
+                div.style.backgroundColor = '#252422';
+            })
+        });
         div.addEventListener('mouseover', ()=> {
-            div.style.backgroundColor = 'black';
+            div.style.backgroundColor = '#252422';
         })
     }
 };
@@ -21,8 +33,24 @@ function resetGrid() {
     gridContainer.innerHTML = '';
 };
 createGrid(defaultSize);
+
 btnSize.addEventListener('click', ()=> {
     chosenSize = prompt('What size do you prefer the square grid?', 16);
-    resetGrid();
-    createGrid(chosenSize);
-})
+    console.log(chosenSize);
+    if (chosenSize <= 64 && chosenSize > 0) {
+        resetGrid();
+        createGrid(chosenSize);
+    }
+    else if (chosenSize > 64){
+        alert('Maximum value is 64');
+    }
+    else if (chosenSize < 0) {
+        alert('Minimum value is 1');
+    }
+    else if(chosenSize == null) {
+        return;
+    }
+    else {
+        alert('Please type a valid number');
+    }
+});
